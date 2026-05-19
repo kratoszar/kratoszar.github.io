859c64b230e8a3dde9407b66c3f53e295b12db7b
Alberto Rosario Zarco <albertozarco57@gmail.com>
2026-05-19 12:10:57 -0600

    update formulario

--- enviar.php
+++ enviar.php
@@ -1,16 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 1. Recibir y sanitizar los datos
     $nombre = htmlspecialchars($_POST['name']);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $asunto_usuario = htmlspecialchars($_POST['subject']);
     $mensaje = htmlspecialchars($_POST['message']);
     $destinatario = "albertozarco57@gmail.com";
-    $asunto = "Nuevo contacto de: $nombre";
+    $asunto_correo = "Portafolio: " . $asunto_usuario;
     $contenido = "Nombre: $nombre\n";
     $contenido .= "Correo: $email\n\n";
     $contenido .= "Mensaje:\n$mensaje\n";
-    $headers = "From: $email\r\n";
+    $headers = "From: albertozarco57@gmail.com\r\n"; 
     $headers .= "Reply-To: $email\r\n";
-    if (@mail($destinatario, $asunto, $contenido, $headers)) {
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    if (@mail($destinatario, $asunto_correo, $contenido, $headers)) {
         echo "<script>
                 alert('¡Mensaje enviado exitosamente!');
                 window.location.href = 'index.html';
